e14ec747df74581d84423838d7d8ef25412398d1
Anna Shlyaeva <anna.v.shlyaeva@noaa.gov>
2026-04-06 12:52:58 -0600

    rename test file + fix name

--- src/ijedi/State/State.h
+++ src/ijedi/State/State.h
@@ -31,7 +31,7 @@ class Geometry;
 
 class State : public mist::base::State, private util::ObjectCounter<State> {
  public:
-  static std::string classname() { return "jedimpas::State"; }
+  static std::string classname() { return "ijedi::State"; }
 
   State(const Geometry &, const eckit::Configuration &);
   State(const Geometry &, const oops::Variables &, const util::DateTime &, bool initToZero = true);
