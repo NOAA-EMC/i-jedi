2f37a7537b778987ef11444b7f05d609ae3bf66c
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2026-04-06 14:03:20 -0400

    Update src/ijedi/Geometry/mom6/GeometryMOM6.h
    Co-authored-by: Cory Martin <cory.r.martin@noaa.gov>

--- src/ijedi/Geometry/mom6/GeometryMOM6.h
+++ src/ijedi/Geometry/mom6/GeometryMOM6.h
@@ -1,8 +1,3 @@
-// (C) Copyright 2026- NOAA.
-// This software is licensed under the terms of the Creative Commons
-// Attribution-NonCommercial-ShareAlike Licence.
-// See LICENSE file in the top-level directory for details.
-
 #pragma once
 
 #include <cstdint>
