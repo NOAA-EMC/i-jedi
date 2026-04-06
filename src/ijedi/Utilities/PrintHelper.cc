b4ec0fe3493917093a3defca4958ceca99e3062e
Anna Shlyaeva <anna.v.shlyaeva@noaa.gov>
2026-04-06 09:15:57 -0600

    Add a stub for State class

--- src/ijedi/Utilities/PrintHelper.cc
+++ src/ijedi/Utilities/PrintHelper.cc
@@ -7,7 +7,10 @@
 
 #include "ijedi/Utilities/PrintHelper.h"
 
+<<<<<<< HEAD
 #include <algorithm>
+=======
+>>>>>>> a2c8d12 (Add a stub for State class)
 #include <cmath>
 #include <limits>
 #include <tuple>
