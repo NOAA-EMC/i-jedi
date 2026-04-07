4f67facb96ca108b531b8588acea653c2154c0de
danholdaway <danholdaway@users.noreply.github.com>
2026-04-07 17:35:28 +0000

    Add Inc: IO, Dirac, Test, Beautiful Method Dividers

--- src/ijedi/Increment/Increment.cc
+++ src/ijedi/Increment/Increment.cc
@@ -1,15 +1,9 @@
-/*
- * (C) Copyright 2026 UCAR
- *
- * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
- */
-
 #include "ijedi/Increment/Increment.h"
 
 #include <algorithm>
 #include <cmath>
 #include <iomanip>
+#include <memory>
 
 #include "atlas/field.h"
 #include "eckit/config/Configuration.h"
@@ -22,53 +16,148 @@
 
 namespace ijedi {
 
-Increment::Increment(const Geometry & geom, const oops::Variables & vars,
-                     const util::DateTime & time)
-    : mist::base::Increment(geom, vars, time), geom_(geom) {}
+  // -----------------------------------------------------------------------------------------------
 
-Increment::Increment(const Geometry & geom, const Increment & other, const bool ad)
-    : mist::base::Increment(geom, other, ad), geom_(geom) {}
+  Increment::Increment(const Geometry & geom, const oops::Variables & vars,
+                       const util::DateTime & time)
+      : mist::base::Increment(geom, vars, time), geom_(geom) {}
 
-Increment::Increment(const Increment & other, const bool copy)
-    : mist::base::Increment(other, copy), geom_(other.geom_) {}
+  // -----------------------------------------------------------------------------------------------
 
-Increment::~Increment() = default;
+  Increment::Increment(const Geometry & geom, const Increment & other, const bool ad)
+      : mist::base::Increment(geom, other, ad), geom_(geom) {}
 
-Increment & Increment::operator=(const Increment & rhs) {
-  mist::base::Increment::operator=(rhs);
-  return *this;
-}
+  // -----------------------------------------------------------------------------------------------
 
-void Increment::read(const eckit::Configuration & config) {
-  oops::Log::trace() << "ijedi::Increment::read starting" << std::endl;
+  Increment::Increment(const Increment & other, const bool copy)
+      : mist::base::Increment(other, copy), geom_(other.geom_) {}
 
-  // TODO(someone) implement read
+  // -----------------------------------------------------------------------------------------------
 
-  oops::Log::trace() << "ijedi::Increment::read done" << std::endl;
-}
+  Increment::~Increment() = default;
 
-void Increment::write(const eckit::Configuration & config) const {
-  oops::Log::trace() << "ijedi::Increment::write starting" << std::endl;
+  // -----------------------------------------------------------------------------------------------
 
-  // TODO(someone) implement write
-
-  oops::Log::trace() << "ijedi::Increment::write done" << std::endl;
-}
-
-void Increment::print(std::ostream & os) const {
-  os << std::endl
-     << "  Valid time: " << this->validTime() << std::endl
-     << ", nFields = " << this->variables().size();
-
-  const auto & comm = geom_.comm();
-  const auto & fs   = this->fieldSet();
-  for (const auto & var : this->variables()) {
-    const atlas::Field & field            = fs.field(var.name());
-    const auto[globalMin, globalMax, rms] = fieldMinMaxRMS(comm, field);
-    os << std::endl
-       << var.name() << " : " << std::scientific << std::setprecision(16) << "Min=" << globalMin
-       << ", Max=" << globalMax << ", RMS=" << rms;
+  Increment & Increment::operator=(const Increment & rhs) {
+    mist::base::Increment::operator=(rhs);
+    return *this;
   }
-}
+
+  // -----------------------------------------------------------------------------------------------
+
+  void Increment::read(const eckit::Configuration & config) {
+    oops::Log::trace() << "ijedi::Increment::read starting" << std::endl;
+
+    // Create a Parameters object
+    IncrementParameters params;
+    params.deserialize(config);
+
+    // Check that there are IO parameters
+    if (params.io.value() == boost::none ||
+        params.io.value()->ioParameters.value() == nullptr)
+    {
+      throw eckit::BadParameter("ijedi::Increment::read: No IO parameters provided", Here());
+    }
+
+    // Get the polymorphic IO parameters
+    const IoParametersBase &ioParams = *params.io.value()->ioParameters.value();
+
+    // Create the IO object to use
+    // ---------------------------
+    std::unique_ptr<IoBase> io(IoFactory::create(geom_, ioParams));
+
+    // Call read method of child
+    // -------------------------
+    io->readBase(this->fieldSet());
+
+    oops::Log::trace() << "ijedi::Increment::read done" << std::endl;
+  }
+
+  // -----------------------------------------------------------------------------------------------
+
+  void Increment::write(const eckit::Configuration & config) const {
+    oops::Log::trace() << "ijedi::Increment::write starting" << std::endl;
+
+    // Create a Parameters object
+    IncrementParameters params;
+    params.deserialize(config);
+
+    // Check that there are IO parameters
+    if (params.io.value() == boost::none ||
+        params.io.value()->ioParameters.value() == nullptr)
+    {
+      throw eckit::BadParameter("ijedi::Increment::read: No IO parameters provided", Here());
+    }
+
+    // Get the polymorphic IO parameters
+    const IoParametersBase &ioParams = *params.io.value()->ioParameters.value();
+
+    // Create the IO object to use
+    // ---------------------------
+    std::unique_ptr<IoBase> io(IoFactory::create(geom_, ioParams));
+
+    // Call write method of child
+    // --------------------------
+    io->writeBase(this->fieldSet());
+
+    oops::Log::trace() << "ijedi::Increment::write done" << std::endl;
+  }
+
+  // -----------------------------------------------------------------------------------------------
+
+  void Increment::dirac(const eckit::Configuration & config) {
+    oops::Log::trace() << "ijedi::Increment::dirac starting" << std::endl;
+
+    // Create a Parameters object
+    DiracParameters params;
+    params.deserialize(config);
+
+    // Extract parameters
+    const std::vector<std::string> diracFlds = params.diracFlds;
+    const std::vector<int> diracProc = params.diracProc;
+    const std::vector<int> diracHorx = params.diracHorx;
+    const std::vector<int> diracVert = params.diracVert;
+
+    // Assert that all vectors are the same lenght
+    size_t nDiracs = diracFlds.size();
+    ASSERT_MSG(diracProc.size() == nDiracs, "Dirac parameters diracProc incorrect length");
+    ASSERT_MSG(diracHorx.size() == nDiracs, "Dirac parameters diracHorx incorrect length");
+    ASSERT_MSG(diracVert.size() == nDiracs, "Dirac parameters diracVert incorrect length");
+
+    // Set diracs
+    for (size_t i = 0; i < diracFlds.size(); ++i) {
+      const std::string & varName = diracFlds[i];
+      const int proc = diracProc[i];
+      const int horx = diracHorx[i];
+      const int vert = diracVert[i];
+
+      if (this->geom_.comm().rank() == proc) {
+        auto field = this->fieldSet().field(varName);
+        auto view = atlas::array::make_view<double, 2>(field);
+        view(horx, vert) = 1.0;
+      }
+    }
+    oops::Log::trace() << "ijedi::Increment::dirac done" << std::endl;
+  }
+
+  // -----------------------------------------------------------------------------------------------
+
+  void Increment::print(std::ostream & os) const {
+    os << std::endl
+       << "  Valid time: " << this->validTime() << std::endl
+       << ", nFields = " << this->variables().size();
+
+    const auto & comm = geom_.comm();
+    const auto & fs   = this->fieldSet();
+    for (const auto & var : this->variables()) {
+      const atlas::Field & field            = fs.field(var.name());
+      const auto[globalMin, globalMax, rms] = fieldMinMaxRMS(comm, field);
+      os << std::endl
+         << var.name() << " : " << std::scientific << std::setprecision(16) << "Min=" << globalMin
+         << ", Max=" << globalMax << ", RMS=" << rms;
+    }
+  }
+
+  // -----------------------------------------------------------------------------------------------
 
 }  // namespace ijedi
--- src/ijedi/Increment/Increment.h
+++ src/ijedi/Increment/Increment.h
@@ -1,10 +1,3 @@
-/*
- * (C) Copyright 2026 UCAR
- *
- * This software is licensed under the terms of the Apache Licence Version 2.0
- * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
- */
-
 #pragma once
 
 #include <ostream>
@@ -13,40 +6,84 @@
 #include "mist/base/Increment.h"
 #include "oops/util/ObjectCounter.h"
 
+#include "oops/base/ParameterTraitsVariables.h"
+#include "oops/util/parameters/OptionalParameter.h"
+#include "oops/util/parameters/Parameter.h"
+#include "oops/util/parameters/Parameters.h"
+#include "oops/util/parameters/RequiredParameter.h"
+
+#include "ijedi/Io/IoBase.h"
+
 namespace eckit {
-class Configuration;
+  class Configuration;
 }  // namespace eckit
 
 namespace oops {
-class Variables;
+  class Variables;
 }  // namespace oops
 
 namespace util {
-class DateTime;
+  class DateTime;
 }  // namespace util
 
 namespace ijedi {
 
-class Geometry;
+  class Geometry;
 
-class Increment : public mist::base::Increment, private util::ObjectCounter<Increment> {
- public:
-  static std::string classname() { return "ijedi::Increment"; }
+  // -----------------------------------------------------------------------------------------------
 
-  Increment(const Geometry &, const oops::Variables &, const util::DateTime &);
-  Increment(const Geometry &, const Increment &, const bool ad = false);
-  Increment(const Increment &, const bool copy = true);
-  ~Increment();
+  class DiracParameters : public oops::Parameters {
+    OOPS_CONCRETE_PARAMETERS(DiracParameters, Parameters)
+    public:
+    oops::RequiredParameter<std::vector<std::string>> diracFlds{"dirac fields", this};
+    oops::RequiredParameter<std::vector<int>> diracProc{"dirac processors", this};
+    oops::RequiredParameter<std::vector<int>> diracHorx{"dirac horizontal index", this};
+    oops::RequiredParameter<std::vector<int>> diracVert{"dirac vertical level", this};
+  };
 
-  Increment & operator=(const Increment &);
+  // -----------------------------------------------------------------------------------------------
 
-  void read(const eckit::Configuration &);
-  void write(const eckit::Configuration &) const;
+  class IncrementParameters : public oops::Parameters
+  {
+    OOPS_CONCRETE_PARAMETERS(IncrementParameters, Parameters)
+   public:
+    // Io parameters wrapper for polymorphic IO parameters (nested under "io" key)
+    oops::OptionalParameter<IoParametersWrapper> io{"io", this};
+  };
 
- private:
-  void print(std::ostream & os) const override;
+  // -----------------------------------------------------------------------------------------------
 
-  const Geometry & geom_;
-};
+  class IncrementWriteParameters : public oops::Parameters
+  {
+    OOPS_CONCRETE_PARAMETERS(IncrementWriteParameters, Parameters)
+   public:
+    // Io parameters for writing (nested under "io" key)
+    oops::OptionalParameter<IoParametersWrapper> io{"io", this};
+  };
+
+  // -----------------------------------------------------------------------------------------------
+
+  class Increment : public mist::base::Increment, private util::ObjectCounter<Increment> {
+   public:
+    static std::string classname() { return "ijedi::Increment"; }
+
+    Increment(const Geometry &, const oops::Variables &, const util::DateTime &);
+    Increment(const Geometry &, const Increment &, const bool ad = false);
+    Increment(const Increment &, const bool copy = true);
+    ~Increment();
+
+    Increment & operator=(const Increment &);
+
+    void read(const eckit::Configuration &);
+    void write(const eckit::Configuration &) const;
+    void dirac(const eckit::Configuration &);
+
+   private:
+    void print(std::ostream & os) const override;
+
+    const Geometry & geom_;
+  };
+
+// -----------------------------------------------------------------------------------------------
 
 }  // namespace ijedi
