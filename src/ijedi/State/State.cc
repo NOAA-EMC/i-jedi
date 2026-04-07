8df0b10da09297187520194a875e073c2e2d15fb
Dan Holdaway <27729500+danholdaway@users.noreply.github.com>
2026-04-07 12:44:33 +0000

    norms

--- src/ijedi/State/State.cc
+++ src/ijedi/State/State.cc
@@ -33,7 +33,7 @@ namespace ijedi
                           util::DateTime(config.getString("date")), false),
         geom_(geom)
   {
-    // If config has 'analytic init' then call analytic_init, else if config has 'io' then call read, else throw an error
+    // If config has 'analytic init' then call analytic_init, else if config has 'io' then call read
     if (config.has("analytic init")) {
       analytic_init(config);
     } else if (config.has("io")) {
