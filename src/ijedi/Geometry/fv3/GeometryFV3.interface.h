7345bed9def07af726c5908fa78b543504ac9fcc
SamuelDegelia-NOAA <Samuel.Degelia@noaa.gov>
2026-06-24 19:38:38 +0000

    fix coding norm

--- src/ijedi/Geometry/fv3/GeometryFV3.interface.h
+++ src/ijedi/Geometry/fv3/GeometryFV3.interface.h
@@ -24,8 +24,9 @@ namespace ijedi
         void f_fv3_geom_initialize(const eckit::LocalConfiguration &, const eckit::mpi::Comm *);
         void f_fv3_geom_create(const eckit::Configuration &, const eckit::Configuration &,
                                const eckit::mpi::Comm *);
-        void f_fv3_geom_set_and_fill_geometry_fields(const void *, const void *, const char *, int, int,
-                                                     const double *, const double *, const double *,
+        void f_fv3_geom_set_and_fill_geometry_fields(const void *, const void *,
+                                                     const char *, int, int, const double *,
+                                                     const double *, const double *,
                                                      const double *);
     }  // extern "C"
 }  // namespace ijedi
